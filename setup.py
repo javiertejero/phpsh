c0904613ab5034fdda516a022f61cfb182f3bc46
Javier Tejero <jtejero@cink.es>
2013-07-09 10:29:08 +0200

    renaming base folder from src to phpsh
    The purpose is to be able to use pip install pointing to my repo so I can
    automate this package installation within a requirements.txt file.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from subprocess import Popen
 import sys
 import os
 
-sys.path.insert(0, 'src')
+sys.path.insert(0, 'phpsh')
 from phpsh import __version__
 
 if len(sys.argv) > 1 and sys.argv[1] == "build":
     build_root = os.path.dirname(os.path.realpath(__file__))
-    make_dir = os.path.join(build_root, "src/xdebug-clients/geben")
+    make_dir = os.path.join(build_root, "phpsh/xdebug-clients/geben")
     p = Popen(["make", "-C", make_dir])
     os.waitpid(p.pid, 0)
 
@@ -27,7 +27,7 @@ setup(
     author_email="phpsh@googlegroups.com",
     url="http://www.phpsh.org/",
     packages=["phpsh"],
-    package_dir={"phpsh": "src"},
+    package_dir={"phpsh": "phpsh"},
     package_data={"phpsh": ["*.php", "*.el",
                             "xdebug-clients/geben/README",
                             "xdebug-clients/geben/LICENSE",
@@ -36,10 +36,10 @@ setup(
                             "xdebug-clients/geben/geben.elc",
                             "xdebug-clients/geben/help",
                             "xdebug-clients/geben/tree-widget/geben/*.png"]},
-    scripts=["src/phpsh", "src/dbgp-phpsh.py"],
+    scripts=["phpsh/phpsh", "phpsh/dbgp-phpsh.py"],
     data_files=[
-        (config_dir, ["src/rc.php", "src/php_manual.db", "src/config.sample"]),
-        ("man/man1", ["src/doc/phpsh.1"]),
+        (config_dir, ["phpsh/rc.php", "phpsh/php_manual.db", "phpsh/config.sample"]),
+        ("man/man1", ["phpsh/doc/phpsh.1"]),
     ],
     requires=["pysqlite", "readline"],
 )
